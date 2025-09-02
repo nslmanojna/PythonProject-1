55a9a1f4d4d8c7ce4d965e944f0487179f568c46
Manojna Nanduri <nslmanojna@gmail.com>
2025-09-02 10:50:54 +0530

    Initial commit

---
+++ pythonhome.py
@@ -0,0 +1,3 @@
+print("Python is a popular programming language.")
+print("Python can be used on a server to create web applications.")
+print("Hello World!")
---
+++ pythonintro.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("Python is a popular programming language.")
+print("It was created by Guido van Rossum, and released in 1991.")
+print("Python is used for: web development (server-side), software development,mathematics,system scripting.")
