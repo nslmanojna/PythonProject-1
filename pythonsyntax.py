3677b0ceb5b7e8deeab3a24a14815e1ea5f4bdb4
Manojna Nanduri <nslmanojna@gmail.com>
2025-09-02 11:35:23 +0530

    Updated pythongettingstarted.py file

---
+++ pythonsyntax.py
@@ -0,0 +1,17 @@
+#This is a comment.
+print("Hello, World!")
+if 5 > 2:
+  print("Five is greater than two!")
+# if 5 > 2:
+# print("Five is greater than two!")
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+x = 5
+y = "Hello, World!"
+print(type(x))
+print(type(y))
