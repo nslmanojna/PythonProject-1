0d145d9bd2f52b5203f48d085d8303f811577d95
Manojna Nanduri <nslmanojna@gmail.com>
2025-09-11 11:18:42 +0530

    Updated main.py file

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+"""
+main.py - A simple module to demonstrate greeting functionality.
+"""
 
 def print_hi(name):
+    """
+    main.py - A simple module to demonstrate greeting functionality.
+    """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
