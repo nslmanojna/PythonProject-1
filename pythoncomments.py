5bca0b0273dab63a6643f82593bb12d2288b0f8c
Manojna Nanduri <nslmanojna@gmail.com>
2025-09-11 13:26:14 +0530

    Updated pythonstring.py file according to pylint standards

--- pythoncomments.py
+++ pythoncomments.py
@@ -1,7 +1,13 @@
 #This is a single line comment
+"""
+Python comments 
+Single Line Comments - Starts with #
+Multi Line Comments - """ """
+"""
 print("Hello World!")
+
 """
 This is a multi line 
 comment
 """
-print("Hello World!")
+print("Hello Python!")
